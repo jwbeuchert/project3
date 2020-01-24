8304b8d4cc3745d09ecea933c4a538cdf4f941b0
tworoose <51271998+kspaffk@users.noreply.github.com>
2020-01-23 18:37:26 -0700

    update routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router()
 const listRoutes = require("./listRoutes")
 const giftRoutes = require("./giftRoutes")
 const userRoutes = require("./userRoutes")
+const userRoutes = require("./chatRoutes")
 
 router.use("/list", listRoutes)
 router.use("/gift", giftRoutes)
 router.use("/user", userRoutes)
+router.use("/chat", userRoutes)
 
 module.exports = router;
--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 const userController = require("../../controllers/usersdb")
 
-router.route("/users").get(userController.findAll).post(userController.create)
+router.route("/").get(userController.findAll).post(userController.create)
 
 router.route("/:id").get(userController.findById).put(userController.update).delete(userController.remove)
 
