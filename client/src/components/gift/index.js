86825692bd86c7b67f17673c5eb971e386cc639f
Jacob Beuchert <jacobbeuchert@gmail.com>
2020-01-25 13:04:52 -0700

    Adding first part of gift component

--- client/src/components/gift/index.js
+++ client/src/components/gift/index.js
@@ -1,15 +1,23 @@
 import React from "react";
-import axios from "axios";
 
-function Gift() {
+const Gift = (props) => {
+  if (props.noGift) {
 
-  function getGifts() {
-    axios
-    .get("/api/gift").then(giftsDB =>
-      console.log(giftsDB.request.response)
-    )
   }
-  return (<h1 onClick={getGifts} >Gift</h1>)
-}
 
+  return (
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Gift Page</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Gift Description</h6>
+    <p class="card-text">A description of your gift.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+  {!props.noGift ?
+   <button className="btn btn-danger" onClick={e => props.deleteList(e, props.list._id)}>Delete</button>
+   : null }
+</div>
+  )
+}
 export default Gift
