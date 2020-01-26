1c2944256530bc0d25990bcb68748e76869170a6
Matthew Tarbet <goatdaddy08@yahoo.com>
2020-01-26 15:22:45 -0700

    working on gift user page

--- client/src/pages/GiftUser/index.js
+++ client/src/pages/GiftUser/index.js
@@ -1,13 +1,34 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NoResultCard from "../../components/NoResultCard";
+import User from "../../components/UserCard";
 
-class index extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Gift Friend</h2>
-      </div>
-    );
-  }
-}
+const GiftUser = () => {
+  const [user, setUser] = useState(null);
+  const [users, setUsers] = useState(null);
 
-export default index;
+  const getUser = () => {
+    axios
+      .get("/api/user/" + {currentUserId})
+      .then(dbUser => {
+          setUser(dbUser.data)
+        });
+  };
+
+  useEffect(() => getUser(), []);
+
+  return (
+    <div>
+      {user && user.giftees.length > 0 ? (
+        user.giftees.map(giftee => {
+            console.log(giftee)
+          return <User key={giftee._id} giftee={giftee} />;
+        })
+      ) : (
+        <NoResultCard key="none" type={"giftees"} />
+      )}
+    </div>
+  );
+};
+
+export default GiftUser;
