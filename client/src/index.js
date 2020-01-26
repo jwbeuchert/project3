8675ccea8d6f236c18beed33c435b76e0ab2dd8f
Kent Spafford <spafcoding@gmail.com>
2020-01-26 00:50:58 -0700

    user in db, login page working

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// Alias BrowserRouter as Router
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
-// Wrap app with Router component and declare single route that always renders the App component
 ReactDOM.render(
-  <Router>
-    {/* Component rendered (App) as a variable instead of JSX */}
-    <Route component={App} />
-  </Router>,
+  <App />,
   document.getElementById("root")
 );
-// App component is wrapped within the router so all components can interact with React Router
