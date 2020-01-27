7c2a97333f6261795d9079f1f9f224eff87f7d3f
Kent Spafford <spafcoding@gmail.com>
2020-01-27 15:08:32 -0700

    change url to use .env

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -62,7 +62,7 @@ export default class Auth {
     this.userProfile = null;
     this.auth0.logout({
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-      returnTo: "http://localhost:3000"
+      returnTo: process.env.REACT_APP_AUTH0_URL
     });
   };
 
