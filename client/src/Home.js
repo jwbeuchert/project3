fc7cd1e2338b76b29c68cf2765aa7d06295b821c
Kent Spafford <spafcoding@gmail.com>
2020-01-29 17:22:31 -0700

    auth0 to hooks

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { useState, useContext } from "react";
+import { useAuth0 } from "./react-auth0-spa";
+import { UserContext } from "./utils/UserContext"
 import axios from "axios";
-
 
 const giftLinks = [
   {
@@ -14,98 +15,70 @@ const giftLinks = [
   }
 ];
 
-class Home extends Component {
-  state = {
-    link: "",
-    description: "",
-    giftList: []
-  };
+const Home = () => {
+  const { dbUser, setDbUser } = useContext(UserContext);
+  const [gift, setGift] = useState({ link: "", description: "" });
 
-  componentDidMount = () => {
-    // API request
-    console.log("Home props: ", this.props);
-    axios.get(`/api/gift?userEmail=${this.props.user.email}`).then(res => {
-      this.setState({
-        giftList: res.data
-      });
-    });
-  };
 
-  handleChange = event => {
-    this.setState({
+  const handleChange = event => {
+    setGift({
       [event.target.name]: event.target.value
     });
   };
 
-  enterGiftItem = () => {
-    const newItem = {
-      link: this.state.link,
-      description: this.state.description,
-      userEmail: this.props.user.email,
-      name: "zeke"
-    };
-    axios.post("/api/gift", newItem).then(res => {
-      console.log(res);
-      // put axios post data
-      const newList = this.state.giftList;
-      newList.push(res.data);
-      this.setState({
-        giftList: newList
-      });
-    });
+  const enterGiftItem = () => {
+    // axios.post("/api/gift", gift).then(res => {
+    //   console.log(res);
+    // });
   };
 
-  render() {
-    const { isAuthenticated } = this.props.auth;
-    return (
-      <>
+  return (
+    <>
+  <div>{dbUser && dbUser._id}</div>
+      <div className="sub-page-body">
+        <h1 className="sub-page-header">Add To List</h1>
+        <div className="sub-section">
+          <button onClick={enterGiftItem}>Enter Gift Link</button>
+          <form>
+            <input
+              className="form-input2"
+              id="giftItems"
+              name="link"
+              value={gift.link}
+              onChange={handleChange}
+            ></input>
+          </form>
 
-        <div className="sub-page-body">
-          <h1 className="sub-page-header">Add To List</h1>
-          <div className="sub-section">
-            <button onClick={this.enterGiftItem}>Enter Gift Link</button>
-            <form>
-              <input
-                className="form-input2"
-                id="giftItems"
-                name="link"
-                value={this.state.link}
-                onChange={this.handleChange}
-              ></input>
-            </form>
+          <button onClick={enterGiftItem}>Enter Description</button>
+          <form>
+            <input
+              className="form-input2"
+              id="giftDescription"
+              name="description"
+              value={gift.description}
+              onChange={handleChange}
+            ></input>
+          </form>
+        </div>
 
-
-            <button onClick={this.enterGiftItem}>Enter Description</button>
-            <form>
-              <input
-                className="form-input2"
-                id="giftDescription"
-                name="description"
-                value={this.state.description}
-                onChange={this.handleChange}
-              ></input>
-            </form>
-          </div>
-
-          <div className="sub-section">
-            <h5 className="sub-header">Gift List</h5>
-            <div className="sub-container">
-              <div className="card" id="card1">
-                <div className="card-body">
-                  {this.state.giftList.map(item => {
+        <div className="sub-section">
+          <h5 className="sub-header">Gift List</h5>
+          <div className="sub-container">
+            <div className="card" id="card1">
+              <div className="card-body">
+                {/* {.map(item => {
                     return <div>{item.link}</div>;
                   })}
                   {this.state.giftList.map(item => {
-                    return <div>{item.description}</div>;
-                  })}
-                </div>
+                    return <div>{item.description}</div>; */}
+                })}
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 
 export default Home;
