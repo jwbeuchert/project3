1b1b97da0b77d3003ac1e071dfa1cd12b9f5f103
Chris Laughter <chrislaugh@gmail.com>
2020-02-01 14:12:23 -0700

    current gift index

--- client/src/components/gift/index.js
+++ client/src/components/gift/index.js
@@ -1,36 +1,5 @@
 // import React from "react";
 
-<<<<<<< HEAD
-// const Gift = props => {
-//   if (props.noGift) {
-//   }
-
-//   return (
-//     <div class="card" style="width: 18rem;">
-//       <div class="card-body">
-//         <h5 class="card-title">Gift Page</h5>
-//         <h6 class="card-subtitle mb-2 text-muted">Gift Description</h6>
-//         <p class="card-text">A description of your gift.</p>
-//         <a href="#" class="card-link">
-//           Card link
-//         </a>
-//         <a href="#" class="card-link">
-//           Another link
-//         </a>
-//       </div>
-//       {!props.noGift ? (
-//         <button
-//           className="btn btn-danger"
-//           onClick={e => props.deleteList(e, props.list._id)}
-//         >
-//           Delete
-//         </button>
-//       ) : null}
-//     </div>
-//   );
-// };
-// export default Gift;
-=======
 const Gift = props => {
   return (
     <div class="card sub-card">
@@ -47,4 +16,3 @@ const Gift = props => {
   );
 };
 export default Gift;
->>>>>>> c1ec740c448f312e8c170e84c2a31aaad7f6c6bb
