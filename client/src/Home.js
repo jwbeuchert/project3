cfd8b5c63d526374e50c5dab880ff2dc2bedf691
Chris Laughter <chrislaugh@gmail.com>
2020-02-01 10:34:52 -0700

    slight styling change

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,18 +2,6 @@ import React, { useState, useContext } from "react";
 import { UserContext } from "./utils/UserContext";
 import axios from "axios";
 
-const giftLinks = [
-  {
-    Image: ""
-  },
-  {
-    link: ""
-  },
-  {
-    description: ""
-  }
-];
-
 const Home = () => {
   const { dbUser, setDbUser } = useContext(UserContext);
   const [giftName, setGiftName] = useState("");
@@ -22,7 +10,6 @@ const Home = () => {
   const [giftCost, setGiftCost] = useState(0);
 
   const handleChange = e => {
-    
     if (e.target.name === "name") {
       setGiftName(e.target.value);
     } else if (e.target.name === "description") {
@@ -37,8 +24,8 @@ const Home = () => {
     let allGift = dbUser.lists.filter(el => el.name === "All Gifts");
     let allGiftId = allGift[0]._id;
     console.log(allGiftId);
-    let gift = { name: giftName, description: giftDescription, link: giftLink }
-    console.log(gift)
+    let gift = { name: giftName, description: giftDescription, link: giftLink };
+    console.log(gift);
 
     axios.post("/api/gift/" + allGiftId, gift).then(res => {
       console.log(res);
@@ -47,14 +34,16 @@ const Home = () => {
 
   return (
     <>
-      <div>{dbUser && dbUser._id}</div>
       <div className="sub-page-body">
-        <h1 className="sub-page-header">Add To List</h1>
         <div className="sub-section">
-          {dbUser &&
-            dbUser.lists.map(list =>
-              list.gifts.map(gift => <h1>{gift.name} q</h1>)
-            )}
+          <h1 className="sub-page-header">Add To List</h1>
+        </div>
+      </div>
+      <div className="sub-page-body">
+        <div className="sub-section">
+          <h4>
+            Copy and paste the link (address bar) of the gift you want to add
+          </h4>
           <form>
             <input
               className="form-input2"
@@ -63,6 +52,7 @@ const Home = () => {
               value={giftLink}
               onChange={e => handleChange(e)}
             ></input>
+            <h4>Paste a description if desired</h4>
             <input
               className="form-input2"
               id="description"
@@ -70,6 +60,7 @@ const Home = () => {
               value={giftDescription}
               onChange={e => handleChange(e)}
             ></input>
+            <h4>Cost of item</h4>
             <input
               className="form-input2"
               id="name"
@@ -80,16 +71,27 @@ const Home = () => {
             <button onClick={e => enterGiftItem(e)}>Enter Gift Link</button>
           </form>
         </div>
+      </div>
 
+      <div className="sub-page-body">
         <div className="sub-section">
           <h5 className="sub-header">Gift List</h5>
           <div className="sub-container">
             <div className="card" id="card1">
-              <div className="card-body">
-              </div>
+              <div className="card-body"></div>
             </div>
           </div>
         </div>
+      </div>
+      <div className="uToken">
+        {dbUser && dbUser._id}
+        {/* <div className="sub-page-body">
+        <div className="sub-section"> */}
+        {/* <h1 className="sub-page-header">Add To List</h1> */}
+        {dbUser &&
+          dbUser.lists.map(list =>
+            list.gifts.map(gift => <h1>{gift.name} q</h1>)
+          )}
       </div>
     </>
   );
